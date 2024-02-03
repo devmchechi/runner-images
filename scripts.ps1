df5492adf5e5700f08b26e668cd0869f7ae03422
Dev chechi <devmchechi@gmail.com>
2024-02-03 17:12:07 -0500

    test

--- scripts.ps1
+++ scripts.ps1
@@ -27,112 +27,112 @@ if (-not ((net localgroup Administrators) -contains 'vaultdb')) { exit 1 }
 
 bcdedit.exe /set TESTSIGNING ON
 
-"$env:IMAGE_FOLDER\build\Configure-WindowsDefender.ps1"
-"$env:IMAGE_FOLDER\build\Configure-PowerShell.ps1"
-"$env:IMAGE_FOLDER\build\Install-PowerShellModules.ps1"
+c:\image\build\Configure-WindowsDefender.ps1
+c:\image\build\Configure-PowerShell.ps1
+c:\image\build\Install-PowerShellModules.ps1
 
-import-module "$env:HELPER_SCRIPT_FOLDER\ImageHelpers.psd1"
+import-module "$env:HELPER_SCRIPT_FOLDER\ImageHelpers\ImageHelpers.psd1"
 import-module "$env:HELPER_SCRIPT_FOLDER\TestsHelpers\TestsHelpers.psm1"
 
-"$env:IMAGE_FOLDER\build\Install-WindowsFeatures.ps1"
-"$env:IMAGE_FOLDER\build\Install-Chocolatey.ps1"
-"$env:IMAGE_FOLDER\build\Configure-BaseImage.ps1"
-"$env:IMAGE_FOLDER\build\Configure-ImageDataFile.ps1"
-"$env:IMAGE_FOLDER\build\Configure-SystemEnvironment.ps1"
-"$env:IMAGE_FOLDER\build\Configure-DotnetSecureChannel.ps1"
+c:\image\build\Install-WindowsFeatures.ps1
+c:\image\build\Install-Chocolatey.ps1
+c:\image\build\Configure-BaseImage.ps1
+c:\image\build\Configure-ImageDataFile.ps1
+c:\image\build\Configure-SystemEnvironment.ps1
+c:\image\build\Configure-DotnetSecureChannel.ps1
 
-"$env:IMAGE_FOLDER\build\Install-VCRedist.ps1"
-"$env:IMAGE_FOLDER\build\Install-Docker.ps1"
-"$env:IMAGE_FOLDER\build\Install-DockerWinCred.ps1"
-"$env:IMAGE_FOLDER\build\Install-DockerCompose.ps1"
-"$env:IMAGE_FOLDER\build\Install-PowershellCore.ps1"
-"$env:IMAGE_FOLDER\build\Install-WebPlatformInstaller.ps1"
-"$env:IMAGE_FOLDER\build\Install-Runner.ps1"
-
-
-"$env:IMAGE_FOLDER\build\Install-VisualStudio.ps1"
-"$env:IMAGE_FOLDER\build\Install-KubernetesTools.ps1"
-"$env:IMAGE_FOLDER\build\Install-NET48-devpack.ps1"
+c:\image\build\Install-VCRedist.ps1
+c:\image\build\Install-Docker.ps1
+c:\image\build\Install-DockerWinCred.ps1
+c:\image\build\Install-DockerCompose.ps1
+c:\image\build\Install-PowershellCore.ps1
+c:\image\build\Install-WebPlatformInstaller.ps1
+c:\image\build\Install-Runner.ps1
 
 
-"$env:IMAGE_FOLDER\build\Install-Wix.ps1"
-"$env:IMAGE_FOLDER\build\Install-WDK.ps1"
-"$env:IMAGE_FOLDER\build\Install-VSExtensions.ps1"
-"$env:IMAGE_FOLDER\build\Install-AzureCli.ps1"
-"$env:IMAGE_FOLDER\build\Install-AzureDevOpsCli.ps1"
-"$env:IMAGE_FOLDER\build\Install-ChocolateyPackages.ps1"
-"$env:IMAGE_FOLDER\build\Install-JavaTools.ps1"
-"$env:IMAGE_FOLDER\build\Install-Kotlin.ps1"
-"$env:IMAGE_FOLDER\build\Install-OpenSSL.ps1"
+c:\image\build\Install-VisualStudio.ps1
+c:\image\build\Install-KubernetesTools.ps1
+c:\image\build\Install-NET48-devpack.ps1
+
+
+c:\image\build\Install-Wix.ps1
+c:\image\build\Install-WDK.ps1
+c:\image\build\Install-VSExtensions.ps1
+c:\image\build\Install-AzureCli.ps1
+c:\image\build\Install-AzureDevOpsCli.ps1
+c:\image\build\Install-ChocolateyPackages.ps1
+c:\image\build\Install-JavaTools.ps1
+c:\image\build\Install-Kotlin.ps1
+c:\image\build\Install-OpenSSL.ps1
 
 wmic product where "name like '%%microsoft azure powershell%%'" call uninstall /nointeractive
 
-"$env:IMAGE_FOLDER\build\Install-ActionsCache.ps1"
-"$env:IMAGE_FOLDER\build\Install-Ruby.ps1"
-"$env:IMAGE_FOLDER\build\Install-PyPy.ps1"
-"$env:IMAGE_FOLDER\build\Install-Toolset.ps1"
-"$env:IMAGE_FOLDER\build\Configure-Toolset.ps1"
-"$env:IMAGE_FOLDER\build\Install-NodeJS.ps1"
-"$env:IMAGE_FOLDER\build\Install-AndroidSDK.ps1"
-"$env:IMAGE_FOLDER\build\Install-PowershellAzModules.ps1"
-"$env:IMAGE_FOLDER\build\Install-Pipx.ps1"
-"$env:IMAGE_FOLDER\build\Install-Git.ps1"
-"$env:IMAGE_FOLDER\build\Install-GitHub-CLI.ps1"
-"$env:IMAGE_FOLDER\build\Install-PHP.ps1"
-"$env:IMAGE_FOLDER\build\Install-Rust.ps1"
-"$env:IMAGE_FOLDER\build\Install-Sbt.ps1"
-"$env:IMAGE_FOLDER\build\Install-Chrome.ps1"
-"$env:IMAGE_FOLDER\build\Install-EdgeDriver.ps1"
-"$env:IMAGE_FOLDER\build\Install-Firefox.ps1"
-"$env:IMAGE_FOLDER\build\Install-Selenium.ps1"
-"$env:IMAGE_FOLDER\build\Install-IEWebDriver.ps1"
-"$env:IMAGE_FOLDER\build\Install-Apache.ps1"
-"$env:IMAGE_FOLDER\build\Install-Nginx.ps1"
-"$env:IMAGE_FOLDER\build\Install-Msys2.ps1"
-"$env:IMAGE_FOLDER\build\Install-WinAppDriver.ps1"
-"$env:IMAGE_FOLDER\build\Install-R.ps1"
-"$env:IMAGE_FOLDER\build\Install-AWSTools.ps1"
-"$env:IMAGE_FOLDER\build\Install-DACFx.ps1"
-"$env:IMAGE_FOLDER\build\Install-MysqlCli.ps1"
-"$env:IMAGE_FOLDER\build\Install-SQLPowerShellTools.ps1"
-"$env:IMAGE_FOLDER\build\Install-SQLOLEDBDriver.ps1"
-"$env:IMAGE_FOLDER\build\Install-DotnetSDK.ps1"
-"$env:IMAGE_FOLDER\build\Install-Mingw64.ps1"
-"$env:IMAGE_FOLDER\build\Install-Haskell.ps1"
-"$env:IMAGE_FOLDER\build\Install-Stack.ps1"
-"$env:IMAGE_FOLDER\build\Install-Miniconda.ps1"
-"$env:IMAGE_FOLDER\build\Install-AzureCosmosDbEmulator.ps1"
-"$env:IMAGE_FOLDER\build\Install-Mercurial.ps1"
-"$env:IMAGE_FOLDER\build\Install-Zstd.ps1"
-"$env:IMAGE_FOLDER\build\Install-NSIS.ps1"
-"$env:IMAGE_FOLDER\build\Install-CloudFoundryCli.ps1"
-"$env:IMAGE_FOLDER\build\Install-Vcpkg.ps1"
-"$env:IMAGE_FOLDER\build\Install-PostgreSQL.ps1"
-"$env:IMAGE_FOLDER\build\Install-Bazel.ps1"
-"$env:IMAGE_FOLDER\build\Install-AliyunCli.ps1"
-"$env:IMAGE_FOLDER\build\Install-RootCA.ps1"
-"$env:IMAGE_FOLDER\build\Install-MongoDB.ps1"
-"$env:IMAGE_FOLDER\build\Install-GoogleCloudCLI.ps1"
-"$env:IMAGE_FOLDER\build\Install-CodeQLBundle.ps1"
-"$env:IMAGE_FOLDER\build\Install-BizTalkBuildComponent.ps1"
-"$env:IMAGE_FOLDER\build\Configure-Diagnostics.ps1"
-"$env:IMAGE_FOLDER\build\Configure-DynamicPort.ps1"
-"$env:IMAGE_FOLDER\build\Configure-GDIProcessHandleQuota.ps1"
-"$env:IMAGE_FOLDER\build\Configure-Shell.ps1"
-"$env:IMAGE_FOLDER\build\Configure-DeveloperMode.ps1"
-"$env:IMAGE_FOLDER\build\Install-LLVM.ps1"
+c:\image\build\Install-ActionsCache.ps1
+c:\image\build\Install-Ruby.ps1
+c:\image\build\Install-PyPy.ps1
+c:\image\build\Install-Toolset.ps1
+c:\image\build\Configure-Toolset.ps1
+c:\image\build\Install-NodeJS.ps1
+c:\image\build\Install-AndroidSDK.ps1
+c:\image\build\Install-PowershellAzModules.ps1
+c:\image\build\Install-Pipx.ps1
+c:\image\build\Install-Git.ps1
+c:\image\build\Install-GitHub-CLI.ps1
+c:\image\build\Install-PHP.ps1
+c:\image\build\Install-Rust.ps1
+c:\image\build\Install-Sbt.ps1
+c:\image\build\Install-Chrome.ps1
+c:\image\build\Install-EdgeDriver.ps1
+c:\image\build\Install-Firefox.ps1
+c:\image\build\Install-Selenium.ps1
+c:\image\build\Install-IEWebDriver.ps1
+c:\image\build\Install-Apache.ps1
+c:\image\build\Install-Nginx.ps1
+c:\image\build\Install-Msys2.ps1
+c:\image\build\Install-WinAppDriver.ps1
+c:\image\build\Install-R.ps1
+c:\image\build\Install-AWSTools.ps1
+c:\image\build\Install-DACFx.ps1
+c:\image\build\Install-MysqlCli.ps1
+c:\image\build\Install-SQLPowerShellTools.ps1
+c:\image\build\Install-SQLOLEDBDriver.ps1
+c:\image\build\Install-DotnetSDK.ps1
+c:\image\build\Install-Mingw64.ps1
+c:\image\build\Install-Haskell.ps1
+c:\image\build\Install-Stack.ps1
+c:\image\build\Install-Miniconda.ps1
+c:\image\build\Install-AzureCosmosDbEmulator.ps1
+c:\image\build\Install-Mercurial.ps1
+c:\image\build\Install-Zstd.ps1
+c:\image\build\Install-NSIS.ps1
+c:\image\build\Install-CloudFoundryCli.ps1
+c:\image\build\Install-Vcpkg.ps1
+c:\image\build\Install-PostgreSQL.ps1
+c:\image\build\Install-Bazel.ps1
+c:\image\build\Install-AliyunCli.ps1
+c:\image\build\Install-RootCA.ps1
+c:\image\build\Install-MongoDB.ps1
+c:\image\build\Install-GoogleCloudCLI.ps1
+c:\image\build\Install-CodeQLBundle.ps1
+c:\image\build\Install-BizTalkBuildComponent.ps1
+c:\image\build\Configure-Diagnostics.ps1
+c:\image\build\Configure-DynamicPort.ps1
+c:\image\build\Configure-GDIProcessHandleQuota.ps1
+c:\image\build\Configure-Shell.ps1
+c:\image\build\Configure-DeveloperMode.ps1
+c:\image\build\Install-LLVM.ps1
 
 
-"$env:IMAGE_FOLDER\build\Install-WindowsUpdates.ps1"
+c:\image\build\Install-WindowsUpdates.ps1
 
 powershell -command "& {if ((-not (Get-Process TiWorker.exe -ErrorAction SilentlyContinue)) -and (-not [System.Environment]::HasShutdownStarted) ) { Write-Output 'Restart complete' }}"
 
-"$env:IMAGE_FOLDER\build\Install-WindowsUpdatesAfterReboot.ps1"
-"$env:IMAGE_FOLDER\tests\RunAll-Tests.ps1"
+c:\image\build\Install-WindowsUpdatesAfterReboot.ps1
+c:\image\tests\RunAll-Tests.ps1
 
 
-"$env:IMAGE_FOLDER\build\Configure-System.ps1"
-"$env:IMAGE_FOLDER\build\Configure-User.ps1"
+c:\image\build\Configure-System.ps1
+c:\image\build\Configure-User.ps1
 
 if( Test-Path "$env:SystemRoot\\System32\\Sysprep\\unattend.xml" )
 { 
